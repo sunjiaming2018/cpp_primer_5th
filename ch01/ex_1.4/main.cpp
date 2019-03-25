a493339d10e1571ceae94f6c40fc6ab57190b5b2
sunjiaming2018 <1094652298@qq.com>
2019-03-26 00:04:50 +0800

    打印两数之积

---
+++ ch01/ex_1.4/main.cpp
@@ -0,0 +1,10 @@
+//打印两数相乘
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "输入两个数" << endl;
+    int v1,v2;
+    cin >> v1 >> v2;
+    cout << v1 << "和" << v2 << "两数之积等于" << v1*v2 <<endl;
+    return 0;
+}
