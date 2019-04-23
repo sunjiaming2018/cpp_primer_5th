ad4d694f521e6ac271dbe1ea4312650b9345c643
sunjiaming2018 <1094652298@qq.com>
2019-04-23 22:24:20 +0800

    使用runtime_error丢出异常说明

---
+++ ch05/ex_5.24/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double v1,v2;
+    cout<<"请输入两个数："<<endl;
+    cin>>v1>>v2;
+    if(v2==0){
+        throw runtime_error("除数不能为0");
+    }
+    else{
+        cout<<"v1除以v2等于："<< v1 / v2 <<endl;
+    }
+    return 0;
+}
