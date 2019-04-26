4abb0932812cf311934325799d49029d93834e06
sunjiaming2018 <1094652298@qq.com>
2019-04-26 08:17:58 +0800

    求阶乘

---
+++ ch06/ex_6.6/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int fact(int val)
+{
+    if(val<0)
+        return -1;
+    int ret=1;
+    for(int i=1;i!=val+1;i++)
+        ret*=i;
+    return ret;
+}
+int main() {
+    int num;
+    cout<<"请输入一个数："<<endl;
+    cin>>num;
+    cout<<num<<"的阶乘是："<<fact(num)<<endl;
+    return 0;
+}
