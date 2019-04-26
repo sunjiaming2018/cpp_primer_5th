7c0123d2bd9899c8e8fd12fee392b57a59d787da
sunjiaming2018 <1094652298@qq.com>
2019-04-27 04:12:07 +0800

    Chapter6头文件

---
+++ ch06/ex_6.8/Chapter6.h
@@ -0,0 +1,10 @@
+//
+// Created by sunjiaming on 19-4-27.
+//
+
+#ifndef EX_6_8_CHAPTER6_H
+#define EX_6_8_CHAPTER6_H
+int fact(int );
+double abs1(double );
+double abs2(double );
+#endif //EX_6_8_CHAPTER6_H
