d7f59b9b6d111f55580c7cb1036a8add3bbb40e7
sunjiaming2018 <1094652298@qq.com>
2019-05-13 15:49:57 +0800

    写一个person类

---
+++ ch07/ex_7.4/Person.h
@@ -0,0 +1,16 @@
+//
+// Created by sunjiaming on 19-5-13.
+//
+
+#ifndef EX_7_4_PERSON_H
+#define EX_7_4_PERSON_H
+
+
+class Person {
+private:
+    string name;
+    string adress;
+};
+
+
+#endif //EX_7_4_PERSON_H
