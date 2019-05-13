097c20f297c5fb3b165fb5d329d67159d1e1ed61
sunjiaming2018 <1094652298@qq.com>
2019-05-13 15:56:08 +0800

    添加返回姓名，地址

---
+++ ch07/ex_7.5/Person.h
@@ -0,0 +1,19 @@
+//
+// Created by sunjiaming on 19-5-13.
+//
+
+#ifndef EX_7_5_PERSON_H
+#define EX_7_5_PERSON_H
+
+
+class Person {
+private:
+        string name;
+        string adress;
+public:
+    string getname() const { return name;}
+    string getadress() const { return adress;}
+};
+
+
+#endif //EX_7_5_PERSON_H
